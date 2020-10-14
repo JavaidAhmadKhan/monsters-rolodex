150d87347e07c5cb22d389e77e3e9f042f9afd6b
JavaidAhmadKhan <relaxjavaid@gmail.com>
2020-10-14 10:02:37 +0530

    Updates

---
+++ precache-manifest.17aee8415a49156e76d3e03dd4e0053d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1e1764a83d2d67ae3569664b50664b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d4620303743c2405b12",
+    "url": "/monsters-rolodex/static/css/main.262fe149.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4620303743c2405b12",
+    "url": "/monsters-rolodex/static/js/main.1f02b679.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
